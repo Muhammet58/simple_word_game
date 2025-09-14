249ea68048b33597c7e10e7f2c6954e0ee1ea71d
muhammet <yildirimuhammed058@gamil.com>
2025-09-14 14:52:18 +0300

    Apply .gitignore and remove ignored files

---
+++ simple_scrabble_game.py
@@ -0,0 +1,6 @@
+# 🔹 3. Kelime Oyunu (Scrabble benzeri) (Set + List)
+# Harflerden oluşan bir küme ver.
+# Kullanıcı bu harflerle kelime türetmeye çalışsın.
+# Kelimenin gerçekten bu harflerle yazılabilir olup olmadığını kontrol et (set.issubset).
+# Puanlama sistemi kur: Harf uzunluğuna göre puan.
+# 👉 Kullanılanlar: set, list, str.
